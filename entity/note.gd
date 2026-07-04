f447e6e3b2cb6633560b1579bfe5c919b0ac3dd6
Myafeux <myafeux@gmail.com>
2026-07-04 23:50:04 +0200

    improvements

--- entity/note.gd
+++ entity/note.gd
@@ -3,4 +3,18 @@ extends Area2D
 @export var frame:int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_child(frame).visibility=true
+	get_child(frame).visible=true
+	$Label.text=text
+
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		$Label.visible=true
+
+
+
+
+func _on_body_exited(body: Node2D) -> void:
+	if body is Player:
+		$Label.visible=false
