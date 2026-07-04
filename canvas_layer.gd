6bcebad88067f63c4000a19567bfe8cbff0f0017
Myafeux <myafeux@gmail.com>
2026-07-04 16:42:50 +0200

    light system

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 # en gros c'est un manager pour les labels de l'écran
+@onready var label_key: Label = $VBoxContainer/LabelKey
 
 var maxKey=0 #le nombre de clées en tout
 var nbKey=0 #le nombre de clées qu'a le joueur
@@ -8,8 +9,8 @@ func addKey():
 	maxKey+=1
 func gotKey():
 	nbKey+=1
-	$Label.text="You have "+str(nbKey)+"/"+str(maxKey)+" keys"
+	label_key.text="You have "+str(nbKey)+"/"+str(maxKey)+" keys"
 	if nbKey==maxKey:
-		$Label.text+="\nGo to the door!"
+		label_key.text+="\nGo to the door!"
 		%Player.hasAllKey=true
 	
--- entity/player.gd
+++ entity/player.gd
@@ -5,9 +5,15 @@ var speed = 75.0
 var playerHidden : int = 0
 var playerInSmokeyFOV : bool = false
 var hasAllKey=false
+var isLightOn=false
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move left", "move right", "move up", "move down")
-	
+	if Input.is_action_just_pressed("useMatch"):
+		isLightOn=not isLightOn
+		if isLightOn:
+			$AnimatedSprite2D.play("light")
+		else:
+			$AnimatedSprite2D.play("dark")
 	velocity = direction * speed
 	move_and_slide()
