1bb79270226bdc1efbf1fa066e4cd2be24189ed1
Myafeux <myafeux@gmail.com>
2026-07-04 18:04:15 +0200

    surprised bear

--- entity/enemy.gd
+++ entity/enemy.gd
@@ -3,11 +3,13 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+var onScreen:bool=false
+var surprised:bool=false
 
-
-func _physics_process(delta: float) -> void:
-	pass
-
+func _process(_delta: float) -> void:
+	if onScreen and %Player.isLightOn:
+		$AnimatedSprite2D.play("surprised")
+		$Timer.start()
 
 func _on_area_2d_body_entered(body: Player) -> void:
 	if(body is Player):
@@ -17,3 +19,20 @@ func _on_area_2d_body_entered(body: Player) -> void:
 func _on_area_2d_body_exited(body: Player) -> void:
 	if(body is Player):
 		body.playerInSmokeyFOV = false
+
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	onScreen=true
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	onScreen=false
+
+
+func _on_timer_timeout() -> void:
+	if %Player.playerHidden==0:
+		print("you died")
+		%Player.die()
+	else:
+		print("you survived")
