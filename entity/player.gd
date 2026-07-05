9463db4bf4631a4a6416d385e5b0e2e917f4d1e0
Myafeux <myafeux@gmail.com>
2026-07-05 11:27:20 +0200

    animation player

--- entity/player.gd
+++ entity/player.gd
@@ -7,6 +7,8 @@ var playerHidden : int = 0
 var playerInSmokeyFOV : bool = false
 var hasAllKey:bool=false
 var isLightOn:bool=false
+var animationPeriodRotation:float=0 # pour faire fonctionner l'animation du joueur
+var animationPeriodVertical:float=0 # pour faire fonctionner l'animation du joueur
 
 func _physics_process(_delta: float) -> void:
 	var direction = Input.get_vector("move left", "move right", "move up", "move down")
@@ -26,6 +28,18 @@ func _physics_process(_delta: float) -> void:
 		%sanityBar.value-=sanitySpeed
 		if %sanityBar.value==0:
 			die()
+	if direction!=Vector2.ZERO:
+		animationPeriodRotation+=_delta*45
+		$AnimatedSprite2D.rotation=0.2*sin(animationPeriodRotation)
+		print(rotation)
+	else:
+		$AnimatedSprite2D.rotation=0
+	if isLightOn:
+		animationPeriodVertical+=_delta*10
+	else:
+		animationPeriodVertical+=_delta*3
+	$AnimatedSprite2D.scale.y=1+0.1*sin(animationPeriodVertical)
+	$AnimatedSprite2D.position.y=-4-1*sin(animationPeriodVertical)
 	velocity = direction * speed
 	if playerInSmokeyFOV and playerHidden==0:
 		die()
