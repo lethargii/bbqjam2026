095b97177f02260475e70ba4515ddd54cc973229
Myafeux <myafeux@gmail.com>
2026-07-05 17:58:26 +0200

    animation++

--- Menus/first_frame.gd
+++ Menus/first_frame.gd
@@ -3,9 +3,6 @@ extends CanvasLayer
 func _process(delta: float) -> void:
 	$Sprite2D.position.y-=70*delta
 	if $Sprite2D.position.y<233.0:
-		$Sprite2D2.visible=true
-		if $Timer.is_stopped():
-			$Timer.start()
-
-func _on_timer_timeout() -> void:
-	get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
+		$Sprite2D2.position.y-=500*delta
+		if $Sprite2D2.position.y<300 and $Sprite2D2.position.y>-300:
+			get_tree().change_scene_to_file("res://Menus/main_menu.tscn")
